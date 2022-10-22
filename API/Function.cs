bf84e690d99a61e9fa713ea15f40db0fa080e757
Glen Souza <glenster75@gmail.com>
2022-10-22 14:27:14 -0700

    initial commit

---
+++ API/Function.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+
+namespace HOLA.Loteria.API
+{
+    public static class Function
+    {
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req,
+            [SignalRConnectionInfo(HubName = "HubValue")] SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+    }
+}
