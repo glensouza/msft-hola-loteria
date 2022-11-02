15bd870b51a555657dd6096da3af234c65d9d0cd
Glen Souza <glenster75@gmail.com>
2022-11-02 15:05:29 -0700

    fix auth

--- Web/Program.cs
+++ Web/Program.cs
@@ -3,11 +3,12 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using AzureStaticWebApps.Blazor.Authentication;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddStaticWebAppsAuthentication();
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) })
+    .AddStaticWebAppsAuthentication();
 
 await builder.Build().RunAsync();
